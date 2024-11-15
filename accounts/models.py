256f33fed7623bf3fb73b94fca2e1b529666acce
Sarizeybekk <Sultansrzybk@outlook.com.tr>
2024-11-16 01:43:57 +0300

    feat:created login,register

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
